bca42bc17e72ef6279109133bd26c6bba606e85a
paulevsGitch <paulevs@yandex.ru>
2022-06-04 12:54:10 +0300

    Entity ignores player in creative

---
+++ src/main/java/paulevs/bhcreative/mixin/LivingMixin.java
@@ -0,0 +1,20 @@
+package paulevs.bhcreative.mixin;
+
+import net.minecraft.entity.EntityBase;
+import net.minecraft.entity.Living;
+import net.minecraft.entity.player.PlayerBase;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import paulevs.bhcreative.Creative;
+
+@Mixin(Living.class)
+public class LivingMixin {
+	@Inject(method = "method_928(Lnet/minecraft/entity/EntityBase;)Z", at = @At("HEAD"), cancellable = true)
+	private void creative_method_928(EntityBase entity, CallbackInfoReturnable<Boolean> info) {
+		if (entity instanceof PlayerBase && Creative.isInCreative((PlayerBase) entity)) {
+			info.setReturnValue(false);
+		}
+	}
+}
