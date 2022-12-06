54401cd846d71af76daccc7c395bcc2204616db6
paulevsGitch <paulevs@yandex.ru>
2022-12-06 15:25:54 +0300

    Fix server-side even warning

--- src/main/java/paulevs/bhcreative/listeners/InitListener.java
+++ src/main/java/paulevs/bhcreative/listeners/InitListener.java
@@ -5,6 +5,7 @@ import net.bhapi.client.ClientRegistries;
 import net.bhapi.event.AfterBlockAndItemsEvent;
 import net.bhapi.event.EventListener;
 import net.bhapi.event.EventRegistrationEvent;
+import net.bhapi.registry.CommonRegistries;
 import paulevs.bhcreative.Creative;
 import paulevs.bhcreative.registry.TabRegistry;
 import paulevs.bhcreative.registry.TabRegistryEvent;
@@ -13,7 +14,7 @@ public class InitListener {
 	@EventListener
 	public void afterInit(EventRegistrationEvent event) {
 		if (!BHAPI.isClient()) return;
-		ClientRegistries.EVENT_REGISTRY_POST.put(
+		CommonRegistries.EVENT_REGISTRY.put(
 			TabRegistryEvent.class, () -> new TabRegistryEvent(TabRegistry.INSTANCE::register)
 		);
 	}
@@ -21,6 +22,6 @@ public class InitListener {
 	@EventListener
 	public static void afterInit2(AfterBlockAndItemsEvent event) {
 		Creative.LOGGER.info("Registering creative tabs");
-		BHAPI.processEntryPoints("bhcreative:events", ClientRegistries.EVENT_REGISTRY_POST);
+		BHAPI.processEntryPoints("bhcreative:events", CommonRegistries.EVENT_REGISTRY);
 	}
 }
