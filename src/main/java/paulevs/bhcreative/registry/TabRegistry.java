9e38d15a0c309f3dcd8d6b5935ae4e8ae1f33ef3
paulevsGitch <paulevs@yandex.ru>
2023-08-20 12:49:16 +0300

    Better tab sorting

--- src/main/java/paulevs/bhcreative/registry/TabRegistry.java
+++ src/main/java/paulevs/bhcreative/registry/TabRegistry.java
@@ -1,6 +1,7 @@
 package paulevs.bhcreative.registry;
 
 import net.modificationstation.stationapi.api.registry.Identifier;
+import net.modificationstation.stationapi.api.registry.ModID;
 import paulevs.bhcreative.BHCreative;
 import paulevs.bhcreative.api.CreativeTab;
 
@@ -10,33 +11,26 @@ import java.util.List;
 import java.util.Map;
 
 public class TabRegistry {
-	private static Map<Identifier, CreativeTab> idToTab = new HashMap<>();
-	private static List<CreativeTab> orderedTabs = new ArrayList<>();
+	private static final Map<ModID, List<CreativeTab>> PRE_ORDERED_TABS = new HashMap<>();
+	private static final List<CreativeTab> ORDERED_TABS = new ArrayList<>();
 	
-	public static CreativeTab register(Identifier id, CreativeTab tab) {
-		idToTab.put(id, tab);
-		orderedTabs.add(tab);
-		return tab;
+	public static void register(Identifier id, CreativeTab tab) {
+		PRE_ORDERED_TABS.computeIfAbsent(id.modID, k -> new ArrayList<>()).add(tab);
 	}
 	
 	public static void sort() {
-		orderedTabs.sort((t1, t2) -> {
-			Identifier id1 = t1.getID();
-			Identifier id2 = t2.getID();
-			if (id1.modID == id2.modID) {
-				return id1.modID == BHCreative.MOD_ID ? 0 : id1.id.compareTo(id2.id);
-			}
-			if (id1.modID == BHCreative.MOD_ID) return -1;
-			if (id2.modID == BHCreative.MOD_ID) return 1;
-			return id1.modID.compareTo(id2.modID);
-		});
+		PRE_ORDERED_TABS.keySet().stream().sorted((modID1, modID2) -> {
+			if (modID1 == BHCreative.MOD_ID) return -1;
+			if (modID2 == BHCreative.MOD_ID) return 1;
+			return modID1.compareTo(modID2);
+		}).forEach(id -> ORDERED_TABS.addAll(PRE_ORDERED_TABS.get(id)));
 	}
 	
 	public static CreativeTab getTabByIndex(int index) {
-		return orderedTabs.get(index);
+		return ORDERED_TABS.get(index);
 	}
 	
 	public static int getTabsCount() {
-		return orderedTabs.size();
+		return ORDERED_TABS.size();
 	}
 }
