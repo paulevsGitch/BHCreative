6f0cbe90d78116fc231d1a66b30368011ed2e6ce
paulevsGitch <paulevs@yandex.ru>
2024-01-12 21:02:07 +0300

    Fix for buckets

--- src/main/java/paulevs/bhcreative/mixin/common/BucketItemMixin.java
+++ src/main/java/paulevs/bhcreative/mixin/common/BucketItemMixin.java
@@ -1,6 +1,8 @@
 package paulevs.bhcreative.mixin.common;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.entity.living.player.PlayerEntity;
 import net.minecraft.item.BucketItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -14,7 +16,8 @@ public abstract class BucketItemMixin extends Item {
 	}
 	
 	@ModifyReturnValue(method = "use", at = @At("RETURN"))
-	private ItemStack bhcreative_useBucket(ItemStack original) {
+	private ItemStack bhcreative_useBucket(ItemStack original, @Local PlayerEntity player) {
+		if (!player.creative_isCreative()) return original;
 		original.itemId = this.id;
 		return original;
 	}
