c6e454a9e77b6c8caeefc3f05585435550b45b39
telvarost <telontelvarost@gmail.com>
2025-02-25 18:29:15 -0500

    Give creative player immunity to fire

--- src/main/java/paulevs/bhcreative/mixin/common/PlayerEntityMixin.java
+++ src/main/java/paulevs/bhcreative/mixin/common/PlayerEntityMixin.java
@@ -36,6 +36,12 @@ public abstract class PlayerEntityMixin extends LivingEntity implements Creative
 	@Override
 	public void creative_setCreative(boolean creative) {
 		this.dataTracker.setData(BHCreative.IS_CREATIVE_ID, BHCreative.toByte(creative));
+
+		if (creative) {
+			this.immuneToFire = true;
+		} else {
+			this.immuneToFire = false;
+		}
 	}
 	
 	@Override
