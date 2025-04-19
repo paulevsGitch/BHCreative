56b287a5ed49bd8cc50a3d70e127a871bc092e12
Yunivers <92553545+AITYunivers@users.noreply.github.com>
2025-04-19 16:02:53 -0700

    Fix annoyances relating to entering flight

--- src/main/java/paulevs/bhcreative/mixin/client/AbstractClientPlayerMixin.java
+++ src/main/java/paulevs/bhcreative/mixin/client/AbstractClientPlayerMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.living.player.AbstractClientPlayer;
 import net.minecraft.entity.living.player.PlayerEntity;
 import net.minecraft.level.Level;
 import net.modificationstation.stationapi.api.network.packet.PacketHelper;
+import org.checkerframework.checker.units.qual.A;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -16,15 +17,18 @@ import paulevs.bhcreative.util.IsFlyingPacket;
 
 @Mixin(AbstractClientPlayer.class)
 public abstract class AbstractClientPlayerMixin extends PlayerEntity {
-	@Unique private long creative_timeout;
-	@Unique private long creative_count;
-	
-	@Shadow protected Minecraft minecraft;
-	
+	@Unique
+	private long creative_timeout;
+	@Unique
+	private boolean creative_debounce;
+
+	@Shadow
+	protected Minecraft minecraft;
+
 	public AbstractClientPlayerMixin(Level arg) {
 		super(arg);
 	}
-	
+
 	@Inject(method = "getCanSuffocate", at = @At("HEAD"), cancellable = true)
 	private void creative_getCanSuffocate(int x, int y, int z, CallbackInfoReturnable<Boolean> info) {
 		if (this.creative_isCreative()) {
@@ -32,26 +36,30 @@ public abstract class AbstractClientPlayerMixin extends PlayerEntity {
 			info.cancel();
 		}
 	}
-	
+
 	@Inject(method = "onKeyPressed(IZ)V", at = @At("HEAD"))
 	public void creative_onKeyPress(int key, boolean pressed, CallbackInfo info) {
 		if (key != minecraft.options.jumpKey.key) return;
 		if (!creative_isCreative()) return;
-		
+
 		if (pressed) {
 			long time = System.currentTimeMillis();
 			creative_timeout = time - creative_timeout;
-			if (creative_count > 0 && creative_timeout < 500) {
+			if (creative_debounce && creative_timeout < 500) {
 				boolean flying = !creative_isFlying();
 				creative_setFlying(flying);
 				if (level.isRemote) PacketHelper.send(new IsFlyingPacket(flying));
-				creative_count = 0;
+			} else {
+				creative_timeout = System.currentTimeMillis();
+				creative_debounce = false;
 			}
-			creative_timeout = System.currentTimeMillis();
-			creative_count = 0;
+		} else {
+			creative_debounce = true;
 		}
-		else {
-			creative_count++;
+
+		// Force timeout if the player lands
+		if (onGround && creative_timeout < 500) {
+			creative_timeout += 500;
 		}
 	}
 }
